497e50bf3eac93775f107445d9ddbed54752f02d
aleksandr <aleksandrkashapov59@gmail.com>
2023-12-11 23:05:54 +0500

    Класс Human и интерфейсы CanRun, CanSwim

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+ public interface CanFly{
+   String fly();
+ }
+ public interface CanRun{
+        boolean run();
+ }
+ public interface CanSwim{
+        void Swim();
+ }
+
+}
